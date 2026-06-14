dcfc0129ba340bc81bf917d934e4d61beff6f6e7
Pedro Rickson <pedrorickson95.fa@gmail.com>
2026-06-14 20:06:03 -0300

    Refactor: Detaching userController from user.routes

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,29 +1,89 @@
-import userModel from "../models/userModel.js";
+import express from "express";
 import bcrypt from "bcrypt";
 
-class userController {
-  async list() {
-    return await userModel.list();
-  }
+import userModel from "../models/userModel.js";
+import validateUser from "../utils/validateUser.js";
 
-  async create(user) {
-    // adicionando encriptacao na senha
-    const passwordHash = await bcrypt.hash(user.password, 10);
-    user.password = passwordHash;
-    return await userModel.create(user);
-  }
+export async function list(req,res) {
+    try {
+      const listUsers = await userModel.list()
+      return res.status(200).json(listUsers);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+          error: true,
+          message: "Erro ao listar usuários.",
+        });
+  } 
+}
 
-  async update(id, user) {
-    if (user.password) {
-      user.password = await bcrypt.hash(user.password, 10);
-    }
+export async function create(req,res) {
+    try {
+      const user = req.body;
 
-    return await userModel.update(id, user);
-  }
+      // validate before saving the new user
+      const validation = validateUser(user);
+      if (validation) {
+        return res.status(400).json(validation);
+      }
 
-  async delete(id) {
-    return await userModel.delete(id);
+      // adicionando encriptacao na senha
+      const passwordHash = await bcrypt.hash(user.password, 10);
+      user.password = passwordHash;
+
+      const response = await userModel.create(user);
+      return res.status(201).json(response);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        error: true,
+        message: "Erro ao cadastrar usuário.",
+    });
   }
 }
 
-export default new userController();
+export async function update(req,res) {
+    try {
+      const { id } = req.params;
+      const user = req.body;
+
+      // validate before saving the new client
+      const validation = validateUser(user, true);
+      if (validation) {
+        return res.status(400).json(validation);
+      }
+
+      if (user.password) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+
+      const response = await userModel.update(id, user);
+      return res.status(200).json(response);
+
+    } catch (error) {
+      console.error(error);
+
+      return res.status(500).json({
+        error: true,
+        message: "Erro ao atualizar usuário.",
+      });
+    }
+}
+
+export async function remove(req,res) {
+    try {
+      const { id } = req.params;
+      const response =await userModel.delete(id);
+      return res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+
+      return res.status(500).json({
+        error: true,
+        message: "Erro ao remover usuário.",
+      });
+    }
+  }
+
+
+
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,70 +1,16 @@
 import express from "express";
 const userRouter = express.Router();
-import userController from "../controller/userController.js";
-import validateUser from "../utils/validateUser.js";
+import {create,list,update,remove} from "../controller/userController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 // get, post, put, delete
 // quando alguém acessar tal URL execute essa função.
-userRouter.get("/", authMiddleware, userController.list);
+userRouter.get("/", authMiddleware, list);
 
-userRouter.post("/", async (req, res) => {
-  try {
-    const user = req.body;
+userRouter.post("/",create);
 
-    // validate before saving the new user
-    const validation = validateUser(user);
-    if (validation) {
-      return res.status(400).json(validation);
-    }
+userRouter.put("/:id", authMiddleware,update);
 
-    const response = await userController.create(user);
-    return res.status(201).json(response);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      error: true,
-      message: "Erro ao cadastrar usuário.",
-    });
-  }
-});
-
-userRouter.put("/:id", authMiddleware, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = req.body;
-
-    // validate before saving the new client
-    const validation = validateUser(user, true);
-    if (validation) {
-      return res.status(400).json(validation);
-    }
-
-    const response = await userController.update(id, user);
-    return res.status(200).json(response);
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({
-      error: true,
-      message: "Erro ao atualizar usuário.",
-    });
-  }
-});
-
-userRouter.delete("/:id", authMiddleware, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = await userController.delete(id);
-    return res.status(200).json(response);
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({
-      error: true,
-      message: "Erro ao remover usuário.",
-    });
-  }
-});
+userRouter.delete("/:id", authMiddleware, remove);
 
 export default userRouter;
